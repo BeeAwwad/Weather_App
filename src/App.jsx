393de7c6d00472f6b27e14aec73c9f140e7c0297
BeeAwwad <aosalihu@gmail.com>
2023-06-07 22:10:57 +0100

    Add some styles to the top part of the app

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-      <div id="container" className="bg-dark-home">
+      <div id="container" className="bg-dark-home font-custom p-11 h-full">
         <Search
           location={location}
           setLocation={setLocation}

3e8e0b47f91d988c67a5f07cee11fbd5fd566f8d
BeeAwwad <aosalihu@gmail.com>
2023-06-11 09:15:27 +0100

    Creating styles for medium view 1

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,7 +6,7 @@ function Search(props) {
   return (
     <div className="w-1/2 text-dark-mode flex mx-auto mt-6">
       <input
-        className="w-full px-4 py-2 rounded-3xl text-dark-second placeholder-gray-300 bg-dark-home focus:outline-none focus:bg-dark-second transition-all ease-in delay-200 focus:bg-opacity-20 shadow-xl"
+        className="w-full px-4 py-2 rounded-3xl text-white placeholder-gray-400 bg-dark-home focus:outline-none focus:bg-dark-second transition-all ease-in delay-100 focus:bg-opacity-20 shadow-xl"
         value={location}
         onChange={(event) => setLocation(event.target.value)}
         onKeyDown={searchLocation}
--- src/components/toppart.jsx
+++ src/components/toppart.jsx
@@ -39,50 +39,56 @@ function TopPart(props) {
   ];
 
   return (
-    <div className="text-white flex bg-dark-mode w-full h-full flex-col">
-      <div className="my-auto flex flex-col items-center">
+    <div className="text-white flex bg-dark-mode md:bg-dark-home w-full h-full flex-col">
+      <div className="my-auto flex flex-col md:p-12 md:bg-dark-third items-center">
         <Search
           location={location}
           setLocation={setLocation}
           searchLocation={searchLocation}
         />
-        {city ? (<div className="mt-10 mb-6 mx-5 p-4 text-3xl font-semibold overflow-hidden">
-          <h2 className="animate-fade-up">{city}</h2>
-        </div>) : null}
-        {data.main ? (
-          <div className="m-2 text-lg text-dark-home bg-dark-second py-1 px-4 rounded-full shadow-inner animate-fade-up overflow-hidden">
-            <p className="animate-fade-up">{currentDate}</p>
-          </div>
-        ) : null}
-
-        {data.weather ? (
-          <div className="m-2 p-3 text-2xl overflow-hidden">
-            <p className="animate-fade-up delay-300">{data.weather[0].main}</p>
-          </div>
-        ) : null}
-
-        <div className="relative bottom-3 p-2 h-fit text-9xl font-semi-bold">
+        <div className="my-auto flex flex-col items-center md:bg-white md:flex-row md:mx-auto">
+          <div className="flex flex-col items-center md:bg-dark-mode">
+          {city ? (
+            <div className="mt-8 md:mt-2 mb-6 md:mb-2 mx-5 p-4 text-3xl font-semibold overflow-hidden">
+              <h2 className="animate-fade-up">{city}</h2>
+            </div>
+          ) : null}
           {data.main ? (
-            <div className="animate-fade-up delay-500">
+            <div className="m-2 text-lg text-dark-home bg-dark-second py-1 px-4 rounded-full shadow-inner animate-fade-up overflow-hidden">
+              <p className="animate-fade-up">{currentDate}</p>
+            </div>
+          ) : null}
+
+          {data.weather ? (
+            <div className="m-2 p-3 text-2xl md:hidden overflow-hidden">
+              <p className="animate-fade-up delay-300">
+                {data.weather[0].main}
+              </p>
+            </div>
+          ) : null}
+
+          {data.main ? (
+            <div className="relative bottom-3 p-2 h-fit md:text-8xl text-9xl font-semi-bold animate-fade-up delay-500">
               <span>{data.main.temp.toFixed()}</span>
-              <span className="">{String.fromCharCode(176)}</span>
+              <span>{String.fromCharCode(176)}</span>
             </div>
           ) : null}
-        </div>
-        <div className="overflow-hidden">
-          {data.weather &&
-          data.weather[0].main &&
-          bgColors.find((bg) => bg.name === data.weather[0].main) ? (
-            <div
-              className={`md:w-12 md:h-12 md:bg ${
-                bgColors.find((bg) => bg.name === data.weather[0].main)
-                  .backgroundColor
-              }`}
-            >
-              {/* Your content */}
-            </div>
-          ) : null}
-          <BottomPart data={data} />
+          </div>
+          <div className="overflow-hidden md:ml-10 md:bg-dark-mode">
+            {data.weather &&
+            data.weather[0].main &&
+            bgColors.find((bg) => bg.name === data.weather[0].main) ? (
+              <div
+                className={`md:mx-auto md:w-40 md:h-40 md:bg ${
+                  bgColors.find((bg) => bg.name === data.weather[0].main)
+                    .backgroundColor
+                }`}
+              >
+                {/* Your content */}
+              </div>
+            ) : null}
+            <BottomPart data={data} />
+          </div>
         </div>
       </div>
     </div>
